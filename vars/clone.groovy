f1ac94e6ab103dd096e5b5d1277072a4d71a7d6d
Bhargaw Kumar Singh <137202128+Bhargaw21@users.noreply.github.com>
2025-02-17 13:27:32 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def clone(String url , String branch){
    echo 'Checking out the code...'
-   git url: url, branch: branch
+   git url: ${url}, branch: ${branch}
    echo "Code Cloning successful"
 }
